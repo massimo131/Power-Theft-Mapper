9b7318ed6df513864d4fe841cf6482f4c5613858
Nimesh Wankhede <158222118+massimo131@users.noreply.github.com>
2024-03-23 03:19:11 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -18,41 +18,40 @@ sh = wrkbk.active
 c = sh.cell(row=3, column=1)
 lth = float(c.value)
 
-# Initial latitude and longitude values
-lat3 = 19.198163880068083
-lat4 = float(lat3)
-lon3 = 72.82592142624505
-lon4 = float(lon3)
+# Initial latitude and longitude values (Starting and End Point of the Transmission Line)
+start_lat = float(19.198163880068083)
+start_lon = float(72.82592142624505)
+end_lat = float(19.117338429973362)
+end_lon = float(72.8935775820629)
 
 # Initial bearing value
 bear = 141.48796990531704
 bearing = float(bear)
 
 # Earth radius
-rr = 6371
-R = float(rr)
+R = float(6371)
 
 # Converting bearing to radians
 brng = math.radians(bearing)
 
 # Converting latitude and longitude to radians
-lat1 = math.radians(lat4)
-lon1 = math.radians(lon4)
+lat1 = math.radians(start_lat)
+lon1 = math.radians(start_lon)
 
 # Calculating new latitude and longitude based on distance and bearing
 lat2 = math.asin(math.sin(lat1) * math.cos(lth / R) + math.cos(lat1) * math.sin(lth / R) * math.cos(brng))
 lon2 = lon1 + math.atan2(math.sin(brng) * math.sin(lth / R) * math.cos(lat1), math.cos(lth / R) - math.sin(lat1) * math.sin(lat2))
 
 # Converting radians to degrees
-lat6 = math.degrees(lat2)
-lon6 = math.degrees(lon2)
+theft_lat = math.degrees(lat2)
+theft_lon = math.degrees(lon2)
 
 # Coordinates for plotting on the map
-lat = [19.198163880068083, lat6, 19.117338429973362]
-long = [72.82592142624505, lon6, 72.8935775820629]
+lat = [start_lat, theft_lat, end_lat]
+long = [start_lon, theft_lon, end_lon]
 
 # Creating a Google Map plotter object
-gmapOne = gmplot.GoogleMapPlotter(19.198163880068083, 72.82592142624505, 15)
+gmapOne = gmplot.GoogleMapPlotter(start_lat, start_lon, 15)
 
 # Scatter plotting the coordinates
 gmapOne.scatter(lat, long, size=50, marker=False)
